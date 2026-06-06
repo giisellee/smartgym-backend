e8194e5f4db17f7e63931adf8da8b6ddd4585f58
Giselle Estevam <giselle.estevam09@aluno.ifce.edu.br>
2026-06-06 15:50:59 -0300

    setup inicial

---
+++ smartgym/src/main/java/com/smartgym/smartgym/SmartgymApplication.java
@@ -0,0 +1,13 @@
+package com.smartgym.smartgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartgymApplication.class, args);
+	}
+
+}
---
+++ smartgym/src/test/java/com/smartgym/smartgym/SmartgymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgym.smartgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
